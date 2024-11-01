6751053882c6458691d9e4ead7462ace282ac9bb
lucaseck <117225985+lucaseck@users.noreply.github.com>
2024-11-01 09:34:24 -0500

    fix: add deadline cloud soho script
    Signed-off-by: lucaseck <117225985+lucaseck@users.noreply.github.com>

---
+++ src/deadline/houdini_submitter/soho/deadline_cloud_soho.py
@@ -0,0 +1,7 @@
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+
+import hou
+
+hou.ui.displayMessage(
+    "Please use the 'Submit' button within the Deadline Cloud node to render with AWS Deadline Cloud."
+)
