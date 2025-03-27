03461c990f7f1f65e8e52be97c239d84f806e85b
Mia Battad <133708990+miabatta@users.noreply.github.com>
2025-03-27 22:15:16 +0000

    feat(installer): Add indiviual installer
    Signed-off-by: Mia Battad <133708990+miabatta@users.noreply.github.com>

--- .github/PULL_REQUEST_TEMPLATE.md
+++ .github/PULL_REQUEST_TEMPLATE.md
@@ -20,6 +20,8 @@ See [DEVELOPMENT.md](https://github.com/aws-deadline/deadline-cloud-for-houdini/
 
 #### Please run the integration tests and paste the results below.
 
+#### #### If `installer/` was modified or a file was added/removed from `src/`, then update the installer tests and post the test results below.
+
 ### Was this change documented?
 
 *delete text starting here*
--- depsBundle.sh
+++ depsBundle.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+
+# This script is called in a shell subprocess.
+# If editing this script, please see the security considerations
+# of this invocation method:
+# https://docs.python.org/3/library/subprocess.html#security-considerations
 set -xeuo pipefail
 
 SCRIPT_FOLDER=$(dirname "$0")/scripts
