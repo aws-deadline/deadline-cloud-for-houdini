61d3df002a91c55e1884e8d6e2f6881359f4dfa0
Mia Battad <133708990+miabatta@users.noreply.github.com>
2025-04-07 14:35:43 +0000

    chore: Fix installer & scripts for release
    Signed-off-by: Mia Battad <133708990+miabatta@users.noreply.github.com>

--- depsBundle.sh
+++ depsBundle.sh
@@ -7,18 +7,4 @@
 # https://docs.python.org/3/library/subprocess.html#security-considerations
 set -xeuo pipefail
 
-SCRIPT_FOLDER=$(dirname "$0")/scripts
-
-pushd "${SCRIPT_FOLDER}"
-python deps_bundle.py
-popd
-
-rm -f dependency_bundle/deadline_cloud_for_houdini_submitter-deps-windows.zip
-rm -f dependency_bundle/deadline_cloud_for_houdini_submitter-deps-linux.zip
-rm -f dependency_bundle/deadline_cloud_for_houdini_submitter-deps-macos.zip
-
-mkdir -p dependency_bundle
-
-cp scripts/dependency_bundle/deadline_cloud_for_houdini_submitter-deps.zip dependency_bundle/deadline_cloud_for_houdini_submitter-deps-windows.zip
-cp scripts/dependency_bundle/deadline_cloud_for_houdini_submitter-deps.zip dependency_bundle/deadline_cloud_for_houdini_submitter-deps-linux.zip
-cp scripts/dependency_bundle/deadline_cloud_for_houdini_submitter-deps.zip dependency_bundle/deadline_cloud_for_houdini_submitter-deps-macos.zip
+python3 scripts/deps_bundle.py
--- test/installer/test_installer.py
+++ test/installer/test_installer.py
@@ -186,7 +186,7 @@ def test_default_location(installer_path: Path):
 
 
 @pytest.mark.skipif(
-    os.getenv("CODEBUILD_SRC_DIR") is None,
+    os.getenv("CODEBUILD_SRC_DIR") is None or os.getenv("IS_DEV_BUILD", "false").lower() == "true",
     reason="Only installers built with a license will not be evaluation mode",
 )
 @pytest.mark.skipif(
@@ -267,8 +267,11 @@ class TestLinuxAndMacOS:
     def _validate_posix_permissions(self, installation_path: Path):
         # assists mypy type checking to ignore this on Windows
         assert sys.platform != "win32"
+        # pwd is not available on Windows
+        import pwd
+
         # GIVEN
-        current_user = getpass.getuser()
+        current_user = pwd.getpwuid(os.getuid())[0]  # type: ignore
 
         # WHEN
         bad_perms: defaultdict[Path, list[str]] = defaultdict(list)
@@ -301,6 +304,23 @@ class TestWindows:
         # GIVEN / WHEN / THEN
         self._verify_windows_least_privilege(user_installation)
 
+    def _running_in_container(self) -> bool:
+        """
+        Check to see if the cexecsvc service exists and is running
+        to determine if we're running on a container.
+        """
+        # assists mypy type checking to ignore this on non-Windows
+        assert sys.platform == "win32"
+        import win32service
+        import win32serviceutil
+
+        try:
+            service_status = win32serviceutil.QueryServiceStatus("cexecsvc")
+            return service_status[1] == win32service.SERVICE_RUNNING
+        except win32service.error:
+            # Service doesn't exist, not on a container
+            return False
+
     @pytest.mark.skipif(not _is_admin(), reason="Tests requires admin privileges")
     def test_system_permissions(self, system_installation):
         # GIVEN / WHEN / THEN
@@ -316,7 +336,15 @@ class TestWindows:
 
         # GIVEN
         windows_user = getpass.getuser()
-        builtin_admin_group_sid, _, _ = win32security.LookupAccountName(None, "Administrators")
+
+        if self._running_in_container():
+            # The admin group is different wehen running
+            # in a container.
+            admin_group = "ContainerAdministrator"
+        else:
+            admin_group = "Administrators"
+
+        builtin_admin_group_sid, _, _ = win32security.LookupAccountName(None, admin_group)
         user_sid, _, _ = win32security.LookupAccountName(None, windows_user)
 
         # WHEN
@@ -332,7 +360,7 @@ class TestWindows:
             if _is_admin():
                 if builtin_admin_group_sid != owner_sid:
                     bad_perms[path].append(
-                        f"Expected to be owned by 'Administrators' but got '{win32security.LookupAccountSid(None, owner_sid)}'"
+                        f"Expected to be owned by '{admin_group}' but got '{win32security.LookupAccountSid(None, owner_sid)}'"
                     )
             elif user_sid != owner_sid:
                 bad_perms[path].append(
@@ -431,7 +459,7 @@ class TestSystemInstall:
 
 
 @pytest.mark.skipif(
-    os.getenv("CODEBUILD_SRC_DIR") is None,
+    os.getenv("CODEBUILD_SRC_DIR") is None or os.getenv("IS_DEV_BUILD", "false").lower() == "true",
     reason="Only installers built internally will be signed",
 )
 class TestVerifySigning:
@@ -456,7 +484,12 @@ class TestVerifySigning:
             Number of errors: 1
         """
         # GIVEN
-        signtool = next(glob.iglob("C:/Program Files*/Windows Kits/*/bin/*/x64/signtool.exe"), None)
+        # Check PATH, then SDK installation location
+        signtool = shutil.which("signtool")
+        if not signtool:
+            signtool = next(
+                glob.iglob("C:/Program Files*/Windows Kits/*/bin/*/x64/signtool.exe"), None
+            )
         assert signtool, "signtool not found in expected location"
 
         # WHEN
