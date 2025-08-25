675f79998a6615ee23d16c65354ff711e9ed00c6
Mia Battad <133708990+miabatta@users.noreply.github.com>
2025-08-25 09:42:54 -0500

    test(integ): Add adaptor test case with render dependencies (#295)
    Signed-off-by: Mia Battad <133708990+miabatta@users.noreply.github.com>

--- test/integ/test_houdini_adaptors.py
+++ test/integ/test_houdini_adaptors.py
@@ -66,3 +66,28 @@ class TestAdaptors:
         assert_all_images_close(
             script_location / "wedge_node_test" / "expected_images", tmp_path / "render"
         )
+
+    def test_render_dependencies_adaptor(
+        self, hython_location: Path, script_location: Path, tmp_path: Path
+    ) -> None:
+        scene_name = "test_render_deps.hip"
+
+        run_command(
+            [
+                str(hython_location),
+                str(script_location / "render_dependencies_test" / "_test_hip.py"),
+                "--",
+                str(script_location),
+                str(tmp_path),
+                scene_name,
+                "adaptor",
+            ]
+        )
+        job_params = {"HipFile": f"{str(tmp_path / scene_name)}"}
+        job_template_location = (
+            script_location / "render_dependencies_test" / "expected_job_bundle" / "template.yaml"
+        )
+        run_houdini_adaptor_test(job_template_location, job_params)
+        assert_all_images_close(
+            script_location / "render_dependencies_test" / "expected_images", tmp_path
+        )
